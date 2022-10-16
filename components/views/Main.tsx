6cede3b989442a7c0a5916714a4a69e4362aa3cf
damienteo <damien.teo.ks@gmail.com>
2022-10-17 02:37:11 +0800

    Basic set up for Main page

---
+++ components/views/Main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../layout/Layout";
+import { Header } from "../common";
+
+const MainPage = () => {
+  return (
+    <Layout>
+      <Header text="Welcome to the Thunderdome" />
+    </Layout>
+  );
+};
+
+export default MainPage;
