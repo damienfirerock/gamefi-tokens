37697cd78a20bb0fa5f1487e8a272b4f6e7b9bba
damienteo <damien.teo.ks@gmail.com>
2022-10-17 03:51:32 +0800

    Incorporate getServerSideProps

--- components/views/Main.tsx
+++ components/views/Main.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box, BoxProps, Container, ContainerProps } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 import Layout from "../layout/Layout";
 import { Header } from "../common";
+
+import { IProduct } from "../../interfaces/IProduct";
 
 const StyledBox = styled(Box)<BoxProps>(() => ({
   display: "flex",
@@ -11,20 +13,21 @@ const StyledBox = styled(Box)<BoxProps>(() => ({
 }));
 
 const StyledContainer = styled(Container)<ContainerProps>(({ theme }) => ({
-  marginTop: theme.spacing(3),
+  marginTop: theme.spacing(1),
 }));
 
-const MainPage = () => {
+const MainPage: React.FunctionComponent<{ data: IProduct[] }> = ({ data }) => {
   return (
     <Layout>
       {/* Header */}
       <StyledContainer>
         <StyledBox>
-          <Header text="Catch a Pokemon today" variant="h3" />
+          <Header text="These Pokemon are fighting fit!" variant="h2" />
         </StyledBox>
       </StyledContainer>
 
       {/* Pieces on Sale */}
+      {data.map((element) => element.name)}
     </Layout>
   );
 };
--- pages/main.tsx
+++ pages/main.tsx
@@ -1,3 +1,11 @@
 import MainPage from "../components/views/Main";
 
 export default MainPage;
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(process.env.BACKEND_URL || "");
+  const { data } = await res.json();
+
+  return { props: { data } };
+}
