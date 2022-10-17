e835040e52759e44313466ad59761b9c9579dcae
damienteo <damien.teo.ks@gmail.com>
2022-10-17 11:25:07 +0800

    Add displayOnwer method

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -14,6 +14,7 @@ import { styled } from "@mui/material/styles";
 
 import { IProduct } from "../../interfaces/IProduct";
 import { truncateWalletAddress } from "../../utils/common";
+import { ZERO_ADDRESS } from "../../constants";
 
 const StyledCard = styled(Card)<CardProps>(({ theme }) => ({
   margin: theme.spacing(1),
@@ -30,6 +31,17 @@ const StyledCardActions = styled(CardActions)<CardActionsProps>(() => ({
   justifyContent: "center",
 }));
 
+const displayOwner = (address: string) => {
+  switch (address) {
+    case ZERO_ADDRESS:
+      return "???";
+    case process.env.NEXT_PUBLIC_TOKEN_SALE_CONTRACT_ADDRESS:
+      return "Available";
+    default:
+      return truncateWalletAddress(address);
+  }
+};
+
 const PokemonCard: React.FunctionComponent<IProduct> = (props) => {
   const { name, description, image, tokenId, owner } = props;
 
@@ -39,7 +51,7 @@ const PokemonCard: React.FunctionComponent<IProduct> = (props) => {
         <Typography variant="h4">{description}</Typography>
         <Typography variant="h5">{name}</Typography>
         <Image src={image} alt={image} width={125} height={125} />
-        <Typography>{truncateWalletAddress(owner)}</Typography>
+        <Typography>{displayOwner(owner)}</Typography>
       </StyledCardContent>
       <StyledCardActions>
         <Button
