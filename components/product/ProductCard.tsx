ea9fe2c2de36fdb30a1b541eed1d6b7cd0bceca1
damienteo <damien.teo.ks@gmail.com>
2022-10-19 11:15:11 +0800

    Disable button if purchase pending for that tokenId

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -1,21 +1,25 @@
 import * as React from "react";
 import {
+  Box,
   Card,
   CardProps,
   CardActions,
   CardActionsProps,
   CardContent,
   CardContentProps,
+  CircularProgress,
   Button,
   Typography,
 } from "@mui/material";
 import Image from "next/image";
 import { styled } from "@mui/material/styles";
+import { useSelector } from "react-redux";
 
+import { RootState } from "../../store";
 import { IProduct } from "../../interfaces/IProduct";
 import { truncateString } from "../../utils/common";
-import { ZERO_ADDRESS } from "../../constants";
 import usePurchaseNFT from "../../utils/hooks/usePurchaseNFT";
+import { ZERO_ADDRESS } from "../../constants";
 
 export const StyledCard = styled(Card)<CardProps>(({ theme }) => ({
   margin: theme.spacing(1),
@@ -51,10 +55,16 @@ const PokemonCard: React.FunctionComponent<IProduct> = (props) => {
   const { name, description, image, tokenId, owner } = props;
 
   const { purchaseNFT } = usePurchaseNFT();
+  const transactionsSlice = useSelector(
+    (state: RootState) => state.transactions
+  );
+  const { pendingTransactions } = transactionsSlice;
 
   const handleClick = () => {
     if (tokenId !== undefined) purchaseNFT(tokenId, description, name);
   };
+
+  const isPending = pendingTransactions.some((txn) => txn.tokenId === tokenId);
 
   return (
     <StyledCard variant="outlined">
@@ -65,16 +75,33 @@ const PokemonCard: React.FunctionComponent<IProduct> = (props) => {
         <Typography variant="h6">{displayOwner(owner)}</Typography>
       </StyledCardContent>
       <StyledCardActions>
-        <Button
-          size="large"
-          variant="outlined"
-          disabled={
-            !(owner === process.env.NEXT_PUBLIC_TOKEN_SALE_CONTRACT_ADDRESS)
-          }
-          onClick={handleClick}
-        >
-          Buy
-        </Button>
+        <Box sx={{ position: "relative" }}>
+          <Button
+            size="large"
+            variant="outlined"
+            disabled={
+              !(
+                owner === process.env.NEXT_PUBLIC_TOKEN_SALE_CONTRACT_ADDRESS
+              ) || isPending
+            }
+            onClick={handleClick}
+          >
+            Buy
+          </Button>
+          {isPending && (
+            <CircularProgress
+              size={24}
+              sx={{
+                color: "rgba(60, 60, 60, 0.1)",
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                marginTop: "-12px",
+                marginLeft: "-12px",
+              }}
+            />
+          )}
+        </Box>
       </StyledCardActions>
     </StyledCard>
   );
