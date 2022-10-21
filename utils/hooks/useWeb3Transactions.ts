93c5a37b0c07f35be76eb054b5f2c501fb4ccea6
damienteo <damien.teo.ks@gmail.com>
2022-10-21 18:08:52 +0800

    Add runPreTransactionChecks and depositPokemon method in usePurchaseNFT

--- utils/hooks/useWeb3Transactions.ts
+++ utils/hooks/useWeb3Transactions.ts
@@ -11,6 +11,17 @@ import { TransactionType } from "../../interfaces/ITransaction";
 import useDispatchErrors from "./useDispatchErrors";
 
 const NFTSaleJson = require("../abis/NFTSale.json");
+const PokemonCenterJson = require("../abis/PokemonCenter.json");
+
+interface ITransactionCheck {
+  nextTransaction: {
+    tokenId: number;
+    description: string;
+    name: string;
+    type: TransactionType;
+  };
+  methodOnFailure: (error: any) => void;
+}
 
 const useWeb3Transactions = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -19,30 +30,17 @@ const useWeb3Transactions = () => {
 
   const { ethereum } = window as any;
 
-  const purchaseNFT = async (
-    tokenId: number,
-    description: string,
-    name: string
-  ) => {
+  const runPreTransactionChecks = async (props: ITransactionCheck) => {
     if (!window || !ethereum) {
       sendTransactionError("No wallet installed");
       return;
     }
 
+    const { nextTransaction, methodOnFailure } = props;
+
     const provider = new ethers.providers.Web3Provider(ethereum, "any");
-    const nextTransaction = {
-      tokenId,
-      description,
-      name,
-      type: TransactionType.TokenSalePurchase,
-    };
 
     dispatch(addPendingTransaction(nextTransaction));
-
-    const dispatchAfterFailure = (error: any) => {
-      dispatch(removePendingTransaction(nextTransaction));
-      sendTransactionErrorOnMetaMaskRequest(error);
-    };
 
     let walletAddress;
 
@@ -52,18 +50,46 @@ const useWeb3Transactions = () => {
       });
       walletAddress = accounts[0]; // first account in MetaMask
     } catch (error: any) {
-      dispatchAfterFailure(error);
+      methodOnFailure(error);
       return;
     }
 
     const { chainId } = await provider.getNetwork();
 
     if (chainId !== parseInt(process.env.NEXT_PUBLIC_NETWORK_CHAIN_ID || "")) {
-      dispatchAfterFailure("Please switch to Goerli network");
+      methodOnFailure("Please switch to Goerli network");
       return;
     }
 
     const signer = provider.getSigner(walletAddress);
+
+    return { signer };
+  };
+
+  const purchaseNFT = async (
+    tokenId: number,
+    description: string,
+    name: string
+  ) => {
+    const nextTransaction = {
+      tokenId,
+      description,
+      name,
+      type: TransactionType.TokenSalePurchase,
+    };
+
+    const dispatchAfterFailure = (error: any) => {
+      dispatch(removePendingTransaction(nextTransaction));
+      sendTransactionErrorOnMetaMaskRequest(error);
+    };
+
+    const { signer } =
+      (await runPreTransactionChecks({
+        nextTransaction,
+        methodOnFailure: dispatchAfterFailure,
+      })) || {};
+
+    if (!signer) return; // errors should be caught in runPreTransactionChecks
 
     const contract = new ethers.Contract(
       process.env.NEXT_PUBLIC_TOKEN_SALE_CONTRACT_ADDRESS || "",
@@ -104,7 +130,71 @@ const useWeb3Transactions = () => {
     // }, 10000);
   };
 
-  return { purchaseNFT };
+  const depositPokemon = async (
+    tokenId: number,
+    description: string,
+    name: string
+  ) => {
+    const nextTransaction = {
+      tokenId,
+      description,
+      name,
+      type: TransactionType.StakingDeposit,
+    };
+
+    const dispatchAfterFailure = (error: any) => {
+      dispatch(removePendingTransaction(nextTransaction));
+      sendTransactionErrorOnMetaMaskRequest(error);
+    };
+
+    const { signer } =
+      (await runPreTransactionChecks({
+        nextTransaction,
+        methodOnFailure: dispatchAfterFailure,
+      })) || {};
+
+    if (!signer) return; // errors should be caught in runPreTransactionChecks
+
+    const contract = new ethers.Contract(
+      process.env.NEXT_PUBLIC_POKEMON_CENTER_ADDRESS || "",
+      PokemonCenterJson.abi,
+      signer
+    );
+
+    let transaction;
+    let receipt: any;
+
+    try {
+      transaction = await contract.deposit(tokenId);
+      receipt = await transaction.wait();
+    } catch (error: any) {
+      dispatchAfterFailure(error);
+      return;
+    }
+
+    const { transactionHash, from, to } = receipt || {};
+
+    const dispatchAfterSuccess = () => {
+      dispatch(
+        updateDBAfterTokenSalePurchase({
+          tokenId,
+          txDetails: { transactionHash, from, to },
+        })
+      );
+
+      dispatch(removePendingTransaction(nextTransaction));
+    };
+
+    // await setTimeout(() => {
+    if (receipt) {
+      dispatchAfterSuccess();
+    } else {
+      dispatchAfterFailure("Un-received Transaction");
+    }
+    // }, 10000);
+  };
+
+  return { purchaseNFT, depositPokemon };
 };
 
 export default useWeb3Transactions;
