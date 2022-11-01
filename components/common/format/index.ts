1140602dc355aca2010e2e9962933eed6d4bab79
damienteo <damien.teo.ks@gmail.com>
2022-11-01 22:58:56 +0800

    Remove un-necessary files

--- components/common/format/index.ts
+++ components/common/format/index.ts
@@ -1,5 +1,3 @@
-import Header from './Header'
-import Paragraph from './Paragraph'
-import Section from './Section'
+import Header from "./Header";
 
-export { Header, Paragraph, Section }
+export { Header };
--- components/common/links/index.ts
+++
@@ -1,4 +0,0 @@
-import { ButtonLink } from './ButtonLink'
-import ExternalLink from './ExternalLink'
-
-export { ButtonLink, ExternalLink }
