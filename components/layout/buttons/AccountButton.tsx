fb8a719d776462cad16d7dbafda2eedeadac3e4e
Damien Teo <damien@firerock.sg>
2023-02-02 16:14:32 +0800

    Change network name based on chain id environmental variable

--- components/layout/buttons/AccountButton.tsx
+++ components/layout/buttons/AccountButton.tsx
@@ -1,42 +1,30 @@
 import React from "react";
-import { Box, BoxProps, Typography } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { Typography } from "@mui/material";
 
 import MetaMaskButton from "./common/MetaMaskButton";
 import MenuStyledButton from "./common/MenuStyledButton";
 
 import useConnectWallet from "../../../utils/hooks/useConnectWallet";
 import { truncateString, handleOpenWindow } from "../../../utils/common";
+import CONFIG from "../../../config";
 
-const StyledBox = styled(Box)<BoxProps>(({ theme }) => ({
-  marginBottom: theme.spacing(2),
-}));
+const nextNetwork = () => {
+  switch (CONFIG.NETWORK_CHAIN_ID) {
+    case "80001":
+      return "Mumbai Testnet";
+    case "137":
+      return "Polygon";
+    default:
+      return "Network";
+  }
+};
 
 const AccountButton: React.FunctionComponent = () => {
   const { provider, account, chainId, requestConnect, requestChangeChainId } =
     useConnectWallet();
 
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
-    null
-  );
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
-  const id = open ? "simple-popover" : undefined;
-
   const handleInstallMetamask = () => {
     handleOpenWindow("https://metamask.io/");
-  };
-
-  const handleDirectFaucet = () => {
-    handleOpenWindow("https://goerlifaucet.com/");
   };
 
   if (!provider)
@@ -52,30 +40,20 @@ const AccountButton: React.FunctionComponent = () => {
       <MetaMaskButton handleClick={requestConnect} text="Connect MetaMask" />
     );
 
-  // TODO: Update text and check for issues when live
-  // This is tailored to Eth network,
-  // Need to support Polygon Network
-  if (chainId !== parseInt(process.env.NEXT_PUBLIC_NETWORK_CHAIN_ID || "5"))
+  if (chainId !== parseInt(CONFIG.NETWORK_CHAIN_ID || "80001"))
     return (
       <MetaMaskButton
         handleClick={requestChangeChainId}
-        text="Change to Goerli"
+        text={`Change to ${nextNetwork()}`}
       />
     );
 
   return (
-    <>
-      <MenuStyledButton
-        aria-describedby={id}
-        variant="contained"
-        disabled={true}
-        onClick={handleClick}
-      >
-        <Typography variant="h6" sx={{ marginLeft: 1 }}>
-          {truncateString(account)}
-        </Typography>
-      </MenuStyledButton>
-    </>
+    <MenuStyledButton variant="contained" disabled={true}>
+      <Typography variant="h6" sx={{ marginLeft: 1 }}>
+        {truncateString(account)}
+      </Typography>
+    </MenuStyledButton>
   );
 };
 
