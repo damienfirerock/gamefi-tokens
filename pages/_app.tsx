8692c278792be02fa1ad65052d07e94425a2ca36
Damien Teo <damien@firerock.sg>
2023-03-06 17:42:21 +0800

    Add wagmi config wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,14 @@ import type { AppProps } from "next/app";
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { Provider } from "react-redux";
+import {
+  WagmiConfig,
+  configureChains,
+  createClient,
+  createStorage,
+} from "wagmi";
+import { polygon, polygonMumbai } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
 
 import createEmotionCache from "../utils/createEmotionCache";
 import theme from "../src/theme";
@@ -14,6 +22,21 @@ interface MyAppProps extends AppProps {
 
 const clientSideEmotionCache = createEmotionCache();
 
+const { provider, webSocketProvider } = configureChains(
+  [polygon, polygonMumbai],
+  [publicProvider()]
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+  storage:
+    typeof window !== "undefined"
+      ? createStorage({ storage: window?.localStorage })
+      : undefined,
+});
+
 const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
@@ -22,7 +45,9 @@ const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
       <ThemeProvider theme={theme}>
         <Provider store={store}>
           <CssBaseline />
-          <Component {...pageProps} />
+          <WagmiConfig client={client}>
+            <Component {...pageProps} />
+          </WagmiConfig>
         </Provider>
       </ThemeProvider>
     </CacheProvider>
