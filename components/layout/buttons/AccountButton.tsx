d1bbabe715b3e8abfbaec1a7f110b95910ff0ab4
Damien Teo <damien@firerock.sg>
2023-03-07 11:24:18 +0800

    Add useWeb3Library hook

--- components/layout/buttons/AccountButton.tsx
+++ components/layout/buttons/AccountButton.tsx
@@ -5,6 +5,7 @@ import MetaMaskButton from "./common/MetaMaskButton";
 import MenuStyledButton from "./common/MenuStyledButton";
 
 import useConnectWallet from "../../../utils/hooks/useConnectWallet";
+import useWagmiLibrary from "../../../utils/hooks/useWeb3Account";
 import { truncateString, handleOpenWindow } from "../../../utils/common";
 import CONFIG from "../../../config";
 
@@ -19,25 +20,39 @@ const nextNetwork = () => {
   }
 };
 
+// TODO: Fix for wallet connect not actually connecting
+// TODO: Update useAirdropTransactions
+// TODO: Add useSignTransactions for signatures between Sequence versus Metamask for binding of wallets
+// TODO: Different layout for desktop versus mobile
+
 const AccountButton: React.FunctionComponent = () => {
   const { provider, account, chainId, requestConnect, requestChangeChainId } =
     useConnectWallet();
+
+  const {
+    address,
+    isConnected,
+    sequenceConnect,
+    metamaskConnect,
+    walletConnectConnect,
+    disconnect,
+  } = useWagmiLibrary();
 
   const handleInstallMetamask = () => {
     handleOpenWindow("https://metamask.io/");
   };
 
-  if (!provider)
+  // if (!provider)
+  //   return (
+  //     <MetaMaskButton handleClick={sequenceConnect} text="Install MetaMask" />
+  //   );
+
+  if (!isConnected)
     return (
       <MetaMaskButton
-        handleClick={handleInstallMetamask}
-        text="Install MetaMask"
+        handleClick={walletConnectConnect}
+        text="Connect MetaMask"
       />
-    );
-
-  if (!account)
-    return (
-      <MetaMaskButton handleClick={requestConnect} text="Connect MetaMask" />
     );
 
   if (chainId !== parseInt(CONFIG.NETWORK_CHAIN_ID || "80001"))
@@ -49,11 +64,23 @@ const AccountButton: React.FunctionComponent = () => {
     );
 
   return (
-    <MenuStyledButton variant="contained" disabled={true}>
-      <Typography variant="h6" sx={{ marginLeft: 1 }}>
-        {truncateString(account)}
-      </Typography>
-    </MenuStyledButton>
+    <>
+      <MenuStyledButton variant="contained" disabled={true}>
+        <Typography variant="h6" sx={{ marginLeft: 1 }}>
+          {/* {truncateString(account)} */}
+          {address}
+        </Typography>
+      </MenuStyledButton>
+      <MenuStyledButton
+        variant="contained"
+        // disabled={true}
+        onClick={() => disconnect()}
+      >
+        <Typography variant="h6" sx={{ marginLeft: 1 }}>
+          Disconnect
+        </Typography>
+      </MenuStyledButton>
+    </>
   );
 };
 
---
+++ utils/hooks/useWeb3Account.ts
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+
+import { configureChains } from "wagmi";
+import { polygon, polygonMumbai } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "@wagmi/core/connectors/walletConnect";
+
+const { chains } = configureChains(
+  [polygon, polygonMumbai],
+  [publicProvider()]
+);
+
+const metamaskConnector = new MetaMaskConnector();
+
+// Note: Sequence Wallet Connector uses require
+// Cannot be imported for use straight,
+// unless converted to dynamic import
+// Error Msg: Instead change the require of index.js, to a dynamic import() which is available in all CommonJS modules
+// Unaware of any other solution other than asynchronous import
+// Relevant StackOverFloe Link: https://stackoverflow.com/questions/71804844/how-would-you-fix-an-err-require-esm-error
+const getSequenceConnector = () =>
+  import("@0xsequence/wagmi-connector").then(
+    ({ SequenceConnector }) =>
+      new SequenceConnector({
+        chains,
+        options: {
+          connect: {
+            app: "XY3",
+            networkId: 137,
+          },
+        },
+      })
+  );
+
+// Pending: Metamask update to be compatitble with WalletConnect V2
+// https://github.com/MetaMask/metamask-mobile/issues/3957
+// Currently stated as 18th June
+const walletConnectConnector = new WalletConnectConnector({
+  chains,
+  options: {
+    projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID!,
+    metadata: {
+      name: "wagmi",
+      description: "my wagmi app",
+      url: "https://wagmi.sh",
+      icons: ["https://wagmi.sh/icon.png"],
+    },
+  },
+});
+
+console.log({ HERE: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID });
+
+// Hook to centralise all wagmi imports in one place
+// Especially due to certain workarounds (i.e. require issue for Sequence connector)
+const useWagmiLibrary = () => {
+  const [sequenceConnector, setSequenceConnector] = useState<any | null>(null);
+
+  const { address, isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
+
+  const { connect: metamaskConnect } = useConnect({
+    connector: metamaskConnector,
+  });
+
+  const { connect: sequenceConnect } = useConnect({
+    connector: sequenceConnector,
+  });
+
+  const { connect: walletConnectConnect } = useConnect({
+    connector: walletConnectConnector,
+  });
+
+  const setupSequenceConnector = async () => {
+    const nextSequenceConntactor = await getSequenceConnector();
+    setSequenceConnector(nextSequenceConntactor);
+  };
+
+  useEffect(() => {
+    setupSequenceConnector();
+  }, []);
+
+  return {
+    address,
+    isConnected,
+    sequenceConnect,
+    metamaskConnect,
+    walletConnectConnect,
+    disconnect,
+  };
+};
+
+export default useWagmiLibrary;
