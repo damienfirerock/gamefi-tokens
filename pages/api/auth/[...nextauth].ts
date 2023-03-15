ed7715d43bd3bcfc3ecc44c9494435ebd6a8841a
Damien Teo <damien@firerock.sg>
2023-03-15 14:35:23 +0800

    Add Facebook OAuth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,22 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+import AppleProvider from "next-auth/providers/apple";
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
     }),
-    // ...add more providers here
+    // AppleProvider({
+    //   clientId: process.env.NEXT_PUBLIC_APPLE_CLIENT_ID,
+    //   clientSecret: process.env.NEXT_PUBLIC_APPLE_CLIENT_SECRET,
+    // }),
+    FacebookProvider({
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET,
+    }),
   ],
 };
 
--- types/environment.d.ts
+++ types/environment.d.ts
@@ -2,10 +2,10 @@ namespace NodeJS {
   interface ProcessEnv extends NodeJS.ProcessEnv {
     // APPLE_ID: string;
     // APPLE_TEAM_ID: string;
-    // APPLE_PRIVATE_KEY: string;
-    // APPLE_KEY_ID: string;
-    // FACEBOOK_ID: string;
-    // FACEBOOK_SECRET: string;
+    NEXT_PUBLIC_APPLE_CLIENT_ID: string;
+    NEXT_PUBLIC_APPLE_CLIENT_SECRET: string;
+    NEXT_PUBLIC_FACEBOOK_CLIENT_ID: string;
+    NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET: string;
     NEXT_PUBLIC_GOOGLE_CLIENT_ID: string;
     NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: string;
   }
