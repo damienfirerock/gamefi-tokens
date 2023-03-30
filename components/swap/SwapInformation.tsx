57264e303d1daa63eafdeaa9bfe015ae5b5eb4bb
Damien Teo <damien@firerock.sg>
2023-03-30 10:26:21 +0800

    Update SwapInformation with config and issues

--- components/swap/SwapInformation.tsx
+++ components/swap/SwapInformation.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
-import { Box, BoxProps, Button, Typography } from "@mui/material";
+import React from "react";
+import { Box, BoxProps, Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { SwapWidget } from "@uniswap/widgets";
+import dynamic from "next/dynamic";
+import { Theme } from "@uniswap/widgets";
 import "@uniswap/widgets/fonts.css";
 import { useTranslation } from "next-i18next";
 
@@ -9,7 +10,30 @@ import StyledCircularProgress from "../common/StyledCircularProgress";
 
 import useHasMounted from "../../utils/hooks/useHasMounted";
 import useWeb3React from "../../utils/hooks/web3React/useWeb3React";
-import { NETWORKS_INFO_CONFIG } from "../../constants/networks";
+import { CONTRACT_LIST, NETWORKS_INFO_CONFIG } from "../../constants/networks";
+
+// Causes errors if not dynamic import
+// Ref: https://github.com/Uniswap/widgets/issues/404
+const SwapWidget = dynamic(
+  async () => {
+    const res = await import("@uniswap/widgets");
+    return res.SwapWidget;
+  },
+  { ssr: false }
+);
+
+const theme: Theme = {
+  primary: "#1F4A05",
+  secondary: "#5F7D52",
+  interactive: "#CBD6BA",
+  container: "#D9ECD9",
+  module: "#E9F7DF",
+  accent: "#8E8B78",
+  outline: "#CADDC2",
+  dialog: "#FFF",
+  fontFamily: "Comic Sans MS",
+  borderRadius: { small: 0, medium: 0, large: 0 },
+};
 
 const StyledBox = styled(Box)<BoxProps>(({ theme }) => ({
   display: "flex",
@@ -42,10 +66,35 @@ const AirdropInformation: React.FunctionComponent = () => {
   return (
     <>
       <div className="Uniswap">
+        {/* Note: Sequence Node does not work on Mumbai Testnet, unable to test swaps */}
+        {/* Note: Keep @uniswap/widgets at 2.47.10 for now,
+        2.48.0 onwards does not detect that transaction on wallet has been sent */}
         <SwapWidget
           provider={library}
-          jsonRpcUrlMap={{ [chainId!]: NETWORKS_INFO_CONFIG[chainId!].rpcUrl }}
+          jsonRpcUrlMap={{
+            [chainId!]: [NETWORKS_INFO_CONFIG[chainId!].rpcUrl],
+          }}
           tokenList={NETWORKS_INFO_CONFIG[chainId!].tokenList}
+          defaultInputTokenAddress={CONTRACT_LIST[chainId!].USDC_ADDRESS}
+          defaultOutputTokenAddress={
+            CONTRACT_LIST[chainId!].FIRE_ROCK_GOLD_ADDRESS
+          }
+          // Note: https://github.com/Uniswap/widgets/issues/465
+          // Locale is currently not working
+          locale="zh-CN"
+        />
+
+        <SwapWidget
+          provider={library}
+          jsonRpcUrlMap={{
+            [chainId!]: [NETWORKS_INFO_CONFIG[chainId!].rpcUrl],
+          }}
+          tokenList={NETWORKS_INFO_CONFIG[chainId!].tokenList}
+          defaultInputTokenAddress={CONTRACT_LIST[chainId!].USDC_ADDRESS}
+          defaultOutputTokenAddress={
+            CONTRACT_LIST[chainId!].FIRE_ROCK_GOLD_ADDRESS
+          }
+          theme={theme}
         />
       </div>
     </>
--- constants/networks.ts
+++ constants/networks.ts
@@ -8,7 +8,7 @@ export enum ChainId {
   MUMBAI = 80001,
 }
 
-const CONTRACT_LIST = {
+export const CONTRACT_LIST = {
   [ChainId.MUMBAI]: {
     FIRE_ROCK_GOLD_ADDRESS: "0xb3170835AfebF0F69dfEE0c70Bf0207E269C0ffe",
     USDC_ADDRESS: "0xaC1922b18bD734C928f6Bb122E51b165049aCd40", // This is Test USD not USDC
@@ -30,7 +30,7 @@ const TOKEN_LIST = {
       decimals: 6,
       chainId: ChainId.MUMBAI,
       logoURI:
-        "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png",
+        "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png",
     },
     {
       name: "Firerock Gold",
@@ -39,7 +39,16 @@ const TOKEN_LIST = {
       decimals: 18,
       chainId: ChainId.MUMBAI,
       logoURI:
-        "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png",
+        "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png",
+    },
+    {
+      name: "Ethereum",
+      address: "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa",
+      symbol: "WETH",
+      decimals: 18,
+      chainId: ChainId.MUMBAI,
+      logoURI:
+        "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png",
     },
   ],
   [ChainId.MATIC]: [
@@ -50,7 +59,7 @@ const TOKEN_LIST = {
       decimals: 6,
       chainId: ChainId.MATIC,
       logoURI:
-        "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png",
+        "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png",
     },
     {
       name: "Firerock Gold",
@@ -59,7 +68,7 @@ const TOKEN_LIST = {
       decimals: 18,
       chainId: ChainId.MATIC,
       logoURI:
-        "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png",
+        "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png",
     },
   ],
 };
