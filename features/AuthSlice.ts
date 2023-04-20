ca0fd9449eccf5faac4baab7345ec347472ac551
Damien Teo <damien@firerock.sg>
2023-04-20 11:44:03 +0800

    Add AuthSlice to redux store

--- features/AuthSlice.ts
+++ features/AuthSlice.ts
@@ -1,24 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { Session } from "next-auth/core/types";
 
-const initialState = {
-  isLoggedIn: false,
-  user: null,
+type SliceState = {
+  dialogOpen: boolean;
+  loading: boolean;
+  session: Session | null;
+};
+
+const initialState: SliceState = {
+  dialogOpen: false,
+  loading: true,
+  session: null,
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    login: (state, action) => {
-      state.isLoggedIn = true;
-      state.user = action.payload;
+    setSession: (state, action) => {
+      state.session = action.payload;
     },
-    logout: (state) => {
-      state.isLoggedIn = false;
-      state.user = null;
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+    setDialogOpen: (state) => {
+      state.dialogOpen = true;
+    },
+    setDialogClosed: (state) => {
+      state.dialogOpen = false;
     },
   },
 });
 
-export const { login, logout } = authSlice.actions;
+export const { setSession, setLoading, setDialogOpen, setDialogClosed } =
+  authSlice.actions;
 export default authSlice.reducer;
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import AppleProvider from "next-auth/providers/apple";
+//TODO: next-auth.js.org/providers/credentials
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   providers: [
@@ -13,6 +15,9 @@ export const authOptions = {
     // AppleProvider({
     //   clientId: process.env.NEXT_PUBLIC_APPLE_CLIENT_ID,
     //   clientSecret: process.env.NEXT_PUBLIC_APPLE_CLIENT_SECRET,
+    // privateKey: process.env.NEXT_PUBLIC_APPLE_PRIVATE_KEY,
+    // keyId: process.env.NEXT_PUBLIC_APPLE_KEY_ID,
+    // teamId: process.env.NEXT_PUBLIC_APPLE_TEAM_ID,
     // }),
     FacebookProvider({
       clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
