a58b6253496dadd718855a0e5aa50864a8692773
Damien Teo <damien@firerock.sg>
2023-05-02 15:32:50 +0800

    Add Apple Provider for Next-Auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,14 +11,10 @@ export const authOptions = {
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
     }),
-    // Need to be enrolled in the Apple Developer Program, which costs money
-    // AppleProvider({
-    //   clientId: process.env.NEXT_PUBLIC_APPLE_CLIENT_ID,
-    //   clientSecret: process.env.NEXT_PUBLIC_APPLE_CLIENT_SECRET,
-    // privateKey: process.env.NEXT_PUBLIC_APPLE_PRIVATE_KEY,
-    // keyId: process.env.NEXT_PUBLIC_APPLE_KEY_ID,
-    // teamId: process.env.NEXT_PUBLIC_APPLE_TEAM_ID,
-    // }),
+    AppleProvider({
+      clientId: process.env.NEXT_PUBLIC_APPLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_APPLE_CLIENT_SECRET,
+    }),
     FacebookProvider({
       clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET,
