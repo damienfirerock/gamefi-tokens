84ba0c04013e8240b69ee252301b5f63767e48ea
Damien Teo <damien@firerock.sg>
2023-05-04 14:32:13 +0800

    Add server selection field with autoselect based on query param

--- components/views/CrystalHub.tsx
+++ components/views/CrystalHub.tsx
@@ -1,17 +1,29 @@
-import React, { useEffect } from "react";
-import { Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Typography,
+} from "@mui/material";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 import Layout from "../layout/Layout";
+import AccountButton from "../layout/buttons/AccountButton";
 
 import { AppDispatch, RootState } from "../../store";
 import { setDialogOpen } from "../../features/AuthSlice";
+import useActiveWeb3React from "../../utils/hooks/web3React/useActiveWeb3React";
+
+const MOCK_SERVERS = ["海洋", "正式服1", "测试服1", "YH1", "SG", "A1"];
 
 const CrystalHub: React.FunctionComponent = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { t } = useTranslation("crystal-hub");
+  const { account } = useActiveWeb3React();
 
   const { query } = useRouter();
   const { email, server, type } = query;
@@ -19,30 +31,61 @@ const CrystalHub: React.FunctionComponent = () => {
   const authSlice = useSelector((state: RootState) => state.auth);
   const { session, loading } = authSlice;
 
-  // TODO: Also need game server params, set server selection field below to server
+  const [selectedServer, selectServer] = useState("");
 
-  const handleOpenLoginDialog = () => {
-    dispatch(setDialogOpen());
+  const handleSelectServer = (event: SelectChangeEvent) => {
+    selectServer(event.target.value as string);
   };
 
   useEffect(() => {
-    if (!loading && !session && email && server && type) {
+    const handleOpenLoginDialog = () => {
+      dispatch(setDialogOpen());
+    };
+
+    // In the event of re-direct from game client
+    if (!loading && !session && email && type) {
       handleOpenLoginDialog();
     }
-  }, [email, server, type, session, loading]);
+  }, [email, type, session, loading]);
+
+  useEffect(() => {
+    if (!!server) {
+      selectServer(server.toString());
+    }
+  }, [server]);
 
   return (
     <Layout>
       {/* Header */}
-      <Typography variant="h3" sx={{ marginTop: 5, marginBottom: 2 }}>
+      <Typography variant="h3" sx={{ marginTop: 5 }}>
         {t("crystal-hub:crystal-hub")}
       </Typography>
 
-      {/* TODO: Show Address */}
-      {/* TODO: Server Selection */}
+      {session && !account && <AccountButton />}
+      {/* TODO: Eventually will need to check against account bound wallet */}
+      {!!account && <Typography variant="h4">Wallet: {account}</Typography>}
+
+      {/* Server Selection */}
+      <Box sx={{ marginY: 2 }}>
+        <FormControl sx={{ minWidth: 120 }}>
+          <InputLabel>Server</InputLabel>
+          <Select
+            value={selectedServer}
+            label="Server"
+            onChange={handleSelectServer}
+          >
+            {MOCK_SERVERS.map((server) => (
+              <MenuItem key={server} value={server}>
+                {server}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+
       {/* TODO: Mock FRG Crystal Balance */}
 
-      <Typography variant="h4" sx={{ marginTop: 5, marginBottom: 2 }}>
+      <Typography variant="h4">
         {t("crystal-hub:deposit")}
 
         {/* TODO: Mock FRG Crystal to $FRG Exchange Rate */}
@@ -50,7 +93,7 @@ const CrystalHub: React.FunctionComponent = () => {
         {/* TODO: Sending of $FRG to wallet upon Mock Deposit */}
       </Typography>
 
-      <Typography variant="h4" sx={{ marginTop: 5, marginBottom: 2 }}>
+      <Typography variant="h4">
         {t("crystal-hub:withdraw")}
         {/* TODO: Mock $FRG to $FRG Crystal Exchange Rate */}
         {/* TODO: Fields for FRG Crystal Exchange */}
