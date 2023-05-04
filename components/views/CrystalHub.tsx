474b2e923bd7539149a0d7696cdf2e1b20fc45e9
Damien Teo <damien@firerock.sg>
2023-05-04 14:58:56 +0800

    Minor update to airdrop page

--- components/views/CrystalHub.tsx
+++ components/views/CrystalHub.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import {
   Box,
+  Card,
+  CardProps,
   FormControl,
   InputLabel,
   MenuItem,
@@ -10,6 +12,7 @@ import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { styled } from "@mui/material/styles";
 
 import Layout from "../layout/Layout";
 import AccountButton from "../layout/buttons/AccountButton";
@@ -19,6 +22,11 @@ import { setDialogOpen } from "../../features/AuthSlice";
 import useActiveWeb3React from "../../utils/hooks/web3React/useActiveWeb3React";
 
 const MOCK_SERVERS = ["海洋", "正式服1", "测试服1", "YH1", "SG", "A1"];
+
+export const StyledCard = styled(Card)<CardProps>(({ theme }) => ({
+  margin: theme.spacing(1),
+  padding: theme.spacing(2),
+}));
 
 const CrystalHub: React.FunctionComponent = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -31,7 +39,10 @@ const CrystalHub: React.FunctionComponent = () => {
   const authSlice = useSelector((state: RootState) => state.auth);
   const { session, loading } = authSlice;
 
-  const [selectedServer, selectServer] = useState("");
+  const [selectedServer, selectServer] = useState<string>("");
+  const [mockCrystalBalance, setMockCrystalBalance] = useState<number>(0);
+  const [mockPendingCrystalBalance, setMockPendingCrystalBalance] =
+    useState<number>(0);
 
   const handleSelectServer = (event: SelectChangeEvent) => {
     selectServer(event.target.value as string);
@@ -85,20 +96,25 @@ const CrystalHub: React.FunctionComponent = () => {
 
       {/* TODO: Mock FRG Crystal Balance */}
 
-      <Typography variant="h4">
-        {t("crystal-hub:deposit")}
+      <StyledCard variant="outlined">
+        <StyledCard variant="outlined">
+          <Typography variant="h4">
+            {t("crystal-hub:deposit")}
+            {/* TODO: Mock FRG Crystal to $FRG Exchange Rate */}
+            {/* TODO: Fields for FRG Crystal Exchange */}
+            {/* TODO: Sending of $FRG to wallet upon Mock Deposit */}
+          </Typography>
+        </StyledCard>
 
-        {/* TODO: Mock FRG Crystal to $FRG Exchange Rate */}
-        {/* TODO: Fields for FRG Crystal Exchange */}
-        {/* TODO: Sending of $FRG to wallet upon Mock Deposit */}
-      </Typography>
-
-      <Typography variant="h4">
-        {t("crystal-hub:withdraw")}
-        {/* TODO: Mock $FRG to $FRG Crystal Exchange Rate */}
-        {/* TODO: Fields for FRG Crystal Exchange */}
-        {/* TODO: Sending of $FRG to company wallet, inducing listener to update mock FRG crystal value */}
-      </Typography>
+        <StyledCard variant="outlined">
+          <Typography variant="h4">
+            {t("crystal-hub:withdraw")}
+            {/* TODO: Mock $FRG to $FRG Crystal Exchange Rate */}
+            {/* TODO: Fields for FRG Crystal Exchange */}
+            {/* TODO: Sending of $FRG to company wallet, inducing listener to update mock FRG crystal value */}
+          </Typography>
+        </StyledCard>
+      </StyledCard>
     </Layout>
   );
 };
