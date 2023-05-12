333a367c73ba344f678089c630816deb3e564f85
Damien Teo <damien@firerock.sg>
2023-05-12 10:20:53 +0800

    Add Logo

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -3,24 +3,30 @@ import { AppBar, Box, BoxProps, Toolbar } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import dynamic from "next/dynamic";
 import { useTranslation } from "next-i18next";
-
-import { Header } from "../common";
+import Image from "next/image";
 
 // Decreases First Load from 355kb to 214kb
 const DynamicAccountButton = dynamic(() => import("./buttons/AccountButton"));
 
 const StyledBox = styled(Box)<BoxProps>(() => ({
   flexGrow: 1,
+  justifyContent: "left",
 }));
 
 const NavBar: React.FunctionComponent = () => {
   const { t } = useTranslation("common");
 
   return (
-    <AppBar position="fixed" color="secondary" elevation={5}>
+    <AppBar position="fixed" color="secondary">
       <Toolbar>
         <StyledBox>
-          <Header text={t("contracts-explorer")} variant="h6" />
+          <Box sx={{ position: "relative", height: "50px", width: "50px" }}>
+            <Image
+              alt="Fire Element Logo"
+              src="/火元素LOGO.svg"
+              layout="fill"
+            />
+          </Box>
         </StyledBox>
         <DynamicAccountButton />
       </Toolbar>
