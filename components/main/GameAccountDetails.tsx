38b835b12b02e0150149bacd6868f5e8223dea52
Damien Teo <damien@firerock.sg>
2023-05-22 14:28:29 +0800

    Style GameAccountDetails

--- components/main/GameAccountDetails.tsx
+++ components/main/GameAccountDetails.tsx
@@ -1,34 +1,48 @@
-import React, { useEffect } from "react";
-import { Box, Typography, Paper } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { Avatar, Box, Typography, TypographyProps } from "@mui/material";
 import { useTranslation } from "next-i18next";
+import { useSession } from "next-auth/react";
+import { styled } from "@mui/material/styles";
 
-import { AppDispatch, RootState } from "../../store";
 import useWeb3React from "../../utils/hooks/web3React/useWeb3React";
-import { setLoading } from "../../features/TransactionSlice";
 
-import { NETWORKS_INFO_CONFIG } from "../../constants/networks";
 import { NAV_TEXT_COLOUR } from "../../src/theme";
 
+const DETAILS_COLOUR = "#0B0320";
+
+const stringToColour = (string: string) => {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+};
+
+const Detail = styled(Typography)<TypographyProps>(() => ({
+  display: "inline-block",
+  color: DETAILS_COLOUR,
+  overflowWrap: "break-word",
+  hyphens: "auto",
+  maxWidth: "170px",
+}));
+
 const GameAccountDetails: React.FunctionComponent = () => {
-  const dispatch = useDispatch<AppDispatch>();
   const { t } = useTranslation(["common", "airdrop"]);
 
-  const { account, chainId } = useWeb3React();
-
-  const setupInitial = async () => {
-    dispatch(setLoading(true));
-
-    // await checkWalletBalance();
-
-    dispatch(setLoading(false));
-  };
-
-  useEffect(() => {
-    if (account) {
-      setupInitial();
-    }
-  }, [account]);
+  const { account } = useWeb3React();
+  const { data: session } = useSession();
 
   return (
     <>
@@ -37,6 +51,8 @@ const GameAccountDetails: React.FunctionComponent = () => {
           <Typography variant="h6">{t("common:account-details")}</Typography>
           <Box
             sx={{
+              display: "flex",
+              flexDirection: "row",
               boxShadow: "0px 0px 20px 0.5px #d3d3d3",
               background: "white",
               marginY: "1rem",
@@ -44,17 +60,38 @@ const GameAccountDetails: React.FunctionComponent = () => {
               borderRadius: "0.5rem",
               color: NAV_TEXT_COLOUR,
             }}
-            // elevation={6}
           >
-            <Typography variant="body1">
-              {t("common:address")}: {account}
-            </Typography>
-            <Typography variant="body1">
-              {t("common:address")}: {account}
-            </Typography>
-            <Typography variant="body1">
-              {t("common:address")}: {account}
-            </Typography>
+            <Avatar
+              sx={{
+                width: "99px",
+                height: "99px",
+                bgcolor: stringToColour(session?.user?.email || ""),
+                marginRight: "1rem",
+              }}
+              src="/logo/火元素LOGO.svg"
+            >
+              {session?.user?.email?.split("")[0] || ""}
+            </Avatar>
+            <table>
+              <tr>
+                <td>{t("common:address")}:</td>
+                <td>
+                  <Detail variant="body1">{session?.user?.email}</Detail>
+                </td>
+              </tr>
+              <tr>
+                <td>{t("common:id")}:</td>
+                <td>
+                  <Detail variant="body1">960630 (Mock)</Detail>
+                </td>
+              </tr>
+              <tr>
+                <td>{t("common:grade")}:</td>
+                <td>
+                  <Detail variant="body1">15 (Mock)</Detail>
+                </td>
+              </tr>
+            </table>
           </Box>
         </>
       )}
