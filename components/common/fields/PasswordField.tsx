419c6a1c0c1a8e80898e08748d2515aa68dcf4c0
Damien Teo <damien@firerock.sg>
2023-05-23 14:44:00 +0800

    Add PasswordField common component

---
+++ components/common/fields/PasswordField.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Controller, Control } from "react-hook-form";
+import { IconButton, InputAdornment } from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
+import StyledTextField from "./StyledField";
+
+interface PasswordTextFieldProps {
+  control: Control<any, any>;
+  name: string;
+  label: string;
+}
+
+const PasswordTextField: React.FunctionComponent<PasswordTextFieldProps> = ({
+  control,
+  name,
+  label,
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      defaultValue=""
+      render={({ field }) => (
+        <StyledTextField
+          {...field}
+          name={name}
+          label={label}
+          type={showPassword ? "text" : "password"}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                >
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+};
+
+export default PasswordTextField;
---
+++ components/common/fields/StyledField.tsx
@@ -0,0 +1,19 @@
+import { TextField, TextFieldProps } from "@mui/material";
+
+import { styled } from "@mui/material/styles";
+
+const ALTERNATE_TEXT_COLOR = "#D8D8D8";
+
+const StyledTextField = styled(TextField)<TextFieldProps>({
+  "& label": {
+    color: ALTERNATE_TEXT_COLOR,
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: ALTERNATE_TEXT_COLOR,
+    },
+    marginBottom: "1.2rem",
+  },
+});
+
+export default StyledTextField;
