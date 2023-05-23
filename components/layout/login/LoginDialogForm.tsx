d50634a1a147756609a1b0204e96d9d0e7d9403f
Damien Teo <damien@firerock.sg>
2023-05-23 15:57:00 +0800

    Actions for Changing Login Form

--- components/layout/login/LoginDialogForm.tsx
+++ components/layout/login/LoginDialogForm.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
+import { IconButton } from "@mui/material";
+import ChevronLeftOutlinedIcon from "@mui/icons-material/ChevronLeftOutlined";
 
 import InteractButton from "../../common/InteractButton";
 import PasswordField from "../../common/fields/PasswordField";
@@ -10,8 +12,18 @@ import DefaultField from "../../common/fields/DefaultField";
 import { RootState } from "../../../store";
 import { NAV_TEXT_COLOUR } from "../../../src/theme";
 
+// TODO: Update when localisation is done
+enum FormType {
+  Login = "Login",
+  Register = "Register",
+  ChangePassword = "Change Password",
+}
+
 interface FormFields {
+  email: string;
+  verificationCode: string;
   password: string;
+  repeatPassword: string;
 }
 
 const LoginDialogForm: React.FunctionComponent = () => {
@@ -20,48 +32,84 @@ const LoginDialogForm: React.FunctionComponent = () => {
   const authSlice = useSelector((state: RootState) => state.auth);
   const { loading } = authSlice;
 
+  const [currentForm, setCurrentForm] = useState<FormType>(FormType.Login);
+
+  const handleChangeOption = (option: FormType) => {
+    setCurrentForm(option);
+  };
+
   return (
     <>
+      {currentForm !== FormType.Login && (
+        <Box sx={{ position: "relative" }}>
+          <IconButton
+            onClick={() => handleChangeOption(FormType.Login)}
+            sx={{ position: "absolute", right: 0, bottom: 0 }}
+          >
+            {/* Back */}
+            <ChevronLeftOutlinedIcon color="primary" />
+          </IconButton>
+        </Box>
+      )}
+
       {/* TextFields */}
       <Box sx={{ display: "flex", flexDirection: "column" }}>
         <DefaultField name="email" label="Email" control={control} />
+        {currentForm !== FormType.Login && (
+          <DefaultField
+            name="verificationCode"
+            label="Verification Code"
+            control={control}
+          />
+        )}
         <PasswordField name="password" label="password" control={control} />
+        {currentForm !== FormType.Login && (
+          <PasswordField
+            name="repeatPassword"
+            label="password (repeat)"
+            control={control}
+          />
+        )}
       </Box>
 
-      {/* Additional Player Options */}
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          marginBottom: "1rem",
-        }}
-      >
-        <InteractButton
-          text="注册新账户"
-          method={() => {
-            return null;
+      {/* Options to Register or Change Password */}
+      {currentForm === FormType.Login && (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginBottom: "1rem",
           }}
-          loading={loading}
-          variant="text"
-          size="small"
-          sx={{ color: NAV_TEXT_COLOUR }}
-        />
-        <InteractButton
-          text="忘记密码？"
-          method={() => {
-            return null;
-          }}
-          loading={loading}
-          variant="text"
-          size="small"
-          sx={{ color: NAV_TEXT_COLOUR }}
-        />
-      </Box>
+        >
+          <InteractButton
+            text="注册新账户"
+            method={() => handleChangeOption(FormType.Register)}
+            loading={loading}
+            variant="text"
+            size="small"
+            sx={{ color: NAV_TEXT_COLOUR }}
+          />
+          <InteractButton
+            text="忘记密码？"
+            method={() => handleChangeOption(FormType.ChangePassword)}
+            loading={loading}
+            variant="text"
+            size="small"
+            sx={{ color: NAV_TEXT_COLOUR }}
+          />
+        </Box>
+      )}
 
-      {/* Login Button */}
+      {currentForm === FormType.Register && (
+        <Typography variant="caption" sx={{ color: NAV_TEXT_COLOUR }}>
+          By signing up, you agree to the user policy.
+        </Typography>
+      )}
+
+      {/* Submission Button */}
       <InteractButton
-        text="Login"
+        text={currentForm}
         method={() => {
           return null;
         }}
