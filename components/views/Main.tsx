cffbae3f2a39cb8979a02e117a6e49b8a243c3c7
Damien Teo <damien@firerock.sg>
2023-05-25 13:12:20 +0800

    Foundational changes for Main Page

--- components/views/Main.tsx
+++ components/views/Main.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Box, BoxProps, Link, Typography } from "@mui/material";
+import {
+  Box,
+  BoxProps,
+  Button,
+  Card,
+  Container,
+  Link,
+  Typography,
+} from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { useTranslation } from "next-i18next";
+import Image from "next/image";
 
 import Layout from "../layout/Layout";
 import CONFIG, { CONTRACT_ADDRESSES, ADDRESS_NAMES } from "../../config";
-
-import AccountDetails from "../layout/account/GameAccountDetails";
+import { NAV_TEXT_COLOUR } from "../../src/theme";
 
 const addresses = Object.values(CONTRACT_ADDRESSES);
 
@@ -19,44 +27,124 @@ const Main: React.FunctionComponent = () => {
 
   return (
     <Layout>
-      {/* Header */}
-      <AccountDetails />
-      <ContractsBox>
-        <Typography variant="h3">{t("addresses")}</Typography>
-        {addresses.map((address) => {
-          if (!address) return;
-          return (
-            <Box
-              key={address}
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                marginY: 3,
-              }}
-            >
-              <Typography
-                variant="body2"
+      <Container maxWidth="sm">
+        <Image
+          alt="XY3 Banner"
+          src="/main-page/main-page-placeholder-image.png"
+          layout="responsive"
+          objectFit="contain"
+          width="100%"
+          height="50%"
+        />
+
+        <Typography
+          variant="body2"
+          sx={{ marginY: "1rem", textAlign: "justify" }}
+        >
+          JoM draws inspiration mainly from Eastern mythology, creating an
+          enormous virtual universe of memorable and unique characters. As the
+          player gains access to more characters over time, they can level up
+          their favourite characters and form the ultimate team to defeat the
+          demons (PvE) or other teams of players (PvP).
+        </Typography>
+
+        {/* Install buttons */}
+        <Box>
+          <a href="/" target="_blank" rel="noopener noreferrer">
+            <Image
+              alt="Google Play Store Link"
+              src="/main-page/google-play-badge.png"
+              width={154}
+              height={44}
+              style={{ objectFit: "fill" }}
+            />
+          </a>
+
+          <a href="/" target="_blank" rel="noopener noreferrer">
+            <Image
+              alt="Apple App Store Link"
+              src="/main-page/apple-download-badge.svg"
+              width={154}
+              height={44}
+            />
+          </a>
+        </Box>
+
+        {/* social links */}
+
+        <Card sx={{ marginY: "0.5rem", padding: "0.5rem" }}>
+          <Typography
+            variant="body2"
+            sx={{ marginBottom: "1rem", color: NAV_TEXT_COLOUR }}
+          >
+            Social Links
+          </Typography>
+          <Link href="/" target="_blank" rel="noopener noreferrer">
+            <Image
+              alt="XY3 Banner"
+              src="/main-page/google-play-badge.png"
+              width={154}
+              height={44}
+              style={{ objectFit: "fill" }}
+            />
+          </Link>
+
+          <Link href="/" target="_blank" rel="noopener noreferrer">
+            <Image
+              alt="XY3 Banner"
+              src="/main-page/apple-download-badge.svg"
+              width={154}
+              height={44}
+            />
+          </Link>
+        </Card>
+        {/* Add paper background like in crystal hub */}
+        {/* Add social links buttons */}
+
+        <Button variant="contained" fullWidth sx={{ marginY: "1rem" }}>
+          <Typography variant="body2">Help Center</Typography>
+        </Button>
+
+        <Button variant="contained" fullWidth>
+          <Typography variant="body2">Legal and Privacy</Typography>
+        </Button>
+
+        <ContractsBox>
+          {addresses.map((address) => {
+            if (!address) return;
+            return (
+              <Box
+                key={address}
                 sx={{
-                  display: "inline-flex",
-                  alignItems: "center",
-                  justifyContent: "center",
+                  display: "flex",
+                  flexDirection: "column",
+                  marginY: 3,
                 }}
               >
-                {ADDRESS_NAMES[address]}:
-              </Typography>
-              <Typography variant="body2">
-                <Link
-                  href={`${CONFIG.POLYGONSCAN_URL}${address}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
+                <Typography
+                  variant="body2"
+                  sx={{
+                    display: "inline-flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
                 >
-                  {address}
-                </Link>
-              </Typography>
-            </Box>
-          );
-        })}
-      </ContractsBox>
+                  {ADDRESS_NAMES[address]}:
+                </Typography>
+                <Typography variant="body2">
+                  <Link
+                    href={`${CONFIG.POLYGONSCAN_URL}${address}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {address}
+                  </Link>
+                </Typography>
+              </Box>
+            );
+          })}
+        </ContractsBox>
+      </Container>
     </Layout>
   );
 };
